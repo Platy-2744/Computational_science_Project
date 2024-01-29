8e871d8672186ef1d8330edc6d02e861c75f5fd7
Platy-2744 <jitisakbigilj132109@gmail.com>
2024-01-29 20:23:16 +0700

    test_1

--- Main.py
+++ Main.py
@@ -2,9 +2,10 @@ import pygame
 
 pygame.init()
 pygame.display.set_caption('Computational Science Project')
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((1000,600))
 ranning = True
 screen.fill([0,100,0])
+pygame.draw.rect(screen,[0,0,0],[50,50,100,150],5)
 pygame.display.update()
 while ranning:
     for event in pygame.event.get():
