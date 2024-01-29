ad218be16ec6d43762c6dbdeff3aab41b1ae4827
Platy-2744 <jitisakbigilj132109@gmail.com>
2024-01-29 20:11:39 +0700

    Update Main.py
    test

--- Main.py
+++ Main.py
@@ -4,9 +4,9 @@ pygame.init()
 pygame.display.set_caption('Computational Science Project')
 screen = pygame.display.set_mode((800,600))
 ranning = True
+screen.fill([0,100,0])
+pygame.display.update()
 while ranning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             ranning = False
-
-pygame.display.update()
